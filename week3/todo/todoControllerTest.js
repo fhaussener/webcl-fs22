217ab0fbcae88448e76dc13e05b1fb1da5398e0c
fhaussener <fabio.haussener@gmail.com>
2022-03-26 14:04:37 +0100

    Add controller tests

--- week3/todo/todoControllerTest.js
+++ week3/todo/todoControllerTest.js
@@ -5,22 +5,53 @@ const todoCtrlSuite = Suite("todoCtrl");
 
 todoCtrlSuite.add("todo-ctrl", assert => {
 
-
     const todoController = TodoController();
 
-    assert.is(todoController.numberOfTodos(),     0);
-    assert.is(todoController.numberOfOpenTasks(), 0);
-    assert.is(todoController.openTasksRatio(),    undefined);
+    assert.is(todoController.numberOfTodos(),       0);
+    assert.is(todoController.numberOfOpenTasks(),   0);
+    assert.is(todoController.numberOfClosedTasks(), 0);
+    assert.is(todoController.openTasksRatio(),      undefined);
+    assert.is(todoController.closedTasksRatio(),    undefined);
 
-    todoController.addTodo();
-    assert.is(todoController.numberOfTodos(),     1);
-    assert.is(todoController.numberOfOpenTasks(), 1);
-    assert.is(todoController.openTasksRatio(),    1); // 100%
+    // Plus 1 Todo
+    const openTodo = todoController.addTodo();
+
+    assert.is(todoController.numberOfTodos(),       1);
+    assert.is(todoController.numberOfOpenTasks(),   1);
+    assert.is(todoController.numberOfClosedTasks(), 0);
+    assert.is(todoController.openTasksRatio(),      1); // 100%
+    assert.is(todoController.closedTasksRatio(),    0); // 0%
 
 
+    //Plus 1 Todo und auf done setzten
+    const closedTodo = todoController.addTodo();
+    closedTodo.setDone(true);
 
-    todoController.addTodo();
+    assert.is(todoController.numberOfTodos(),       2);
+    assert.is(todoController.numberOfOpenTasks(),   1);
+    assert.is(todoController.numberOfClosedTasks(), 1);
+    assert.is(todoController.openTasksRatio(),      0.5); // 50%
+    assert.is(todoController.closedTasksRatio(),    0.5); // 50%
 
+
+    //Minus 1 open Todo
+    todoController.removeTodo(openTodo);
+
+    assert.is(todoController.numberOfTodos(),       1);
+    assert.is(todoController.numberOfOpenTasks(),   0);
+    assert.is(todoController.numberOfClosedTasks(), 1);
+    assert.is(todoController.openTasksRatio(),      0); // 0%
+    assert.is(todoController.closedTasksRatio(),    1); // 100%
+
+    
+    //Minus 1 closed Todo
+    todoController.removeTodo(closedTodo);
+
+    assert.is(todoController.numberOfTodos(),       0);
+    assert.is(todoController.numberOfOpenTasks(),   0);
+    assert.is(todoController.numberOfClosedTasks(), 0);
+    assert.is(todoController.openTasksRatio(),      undefined); // 0%
+    assert.is(todoController.closedTasksRatio(),    undefined); // 0%
 });
 
 todoCtrlSuite.run();
